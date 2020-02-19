a8254f6f392294b6274d2df995c9d56324ff7bf4
Administrator <admin@example.com>
2020-02-19 17:04:20 +0200

    go-go

---
+++ 1/simple-test-1.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "eu-west-1"
+
+}
+
+resource "aws_instance" "myfirst_instance" {
+  ami           = "ami-0713f98de93617bb4"
+  instance_type = "t3.micro"
+  tags = {
+    Name  = "super instance"
+    Owner = "Artem Havrylenko"
+    Phone = "+380971652575"
+  }
+}
+
+resource "aws_instance" "mySecond_instance" {
+  count         = 2
+  ami           = "ami-0713f98de93617bb4"
+  instance_type = "t2.micro"
+  tags = {
+    Name  = "super instance 2"
+    Owner = "Artem Havrylenko"
+    Phone = "+380971652575"
+  }
+}
